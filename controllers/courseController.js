146f655f42b7f2a62a310a025ed3a53f8d5fb224
ImALittlePieceOfTheUniverse <civilengineer0106@gmail.com>
2022-09-28 16:59:49 +0300

    Continue on the relationship between student and courses

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,10 +1,15 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
-const user = require('../models/User');
+const User = require('../models/User');
 
 exports.createCourse = async (req, res) => {
   try {
-    const course = await Course.create(req.body);
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      user: req.session.userID,
+    });
     res.status(201).redirect('/courses');
   } catch (error) {
     res.status(400).json({
@@ -43,13 +48,30 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
-    const course = await Course.findOne({ slug: req.params.slug });
+    const course = await Course.findOne({ slug: req.params.slug }).populate(
+      'user'
+    );
     const categories = await Category.find();
     res.status(200).render('course', {
       course,
       page_name: 'courses',
       categories,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'FAIL',
+      error,
+    });
+  }
+};
+
+exports.enrollCourse = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.courses.push({ _id: req.body.course_id });
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'FAIL',
